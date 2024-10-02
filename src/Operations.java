cf81e359ecdbf087f7213c1ee8d6a7debe5799ef
Samuel serpa <sdserpaz@eafit.edu.co>
2024-10-02 08:57:21 -0500

    adding comments

--- src/Operations.java
+++ src/Operations.java
@@ -6,25 +6,25 @@ public class Operations {
 
     private static void entry() {  //the entry() method gets a string whit one operation and modify global string who is used in the operation methods
         System.out.println("enter the expression");
-        string =  scan.nextLine();
+        string =  scan.nextLine(); //get all the line
 
     }
 
     public static void sum(){
         entry();
-        arr = string.split("\\+");
+        arr = string.split("\\+"); //split the string when +
         int summed = Arrays.stream(arr).mapToInt(Integer:: parseInt).reduce(0,Integer::sum);
-        System.out.println(summed);
+        System.out.println(summed); //return
     }
     public static void subtract(){
         entry();
-        arr = string.split("-");
+        arr = string.split("-"); //split the string when "-"
         int subtract = Arrays.stream(arr).mapToInt(Integer:: parseInt).reduce((a,b)->a-b).orElse(0);
         System.out.println(subtract);
     }
     public static void multiplication(){
         entry();
-        arr = string.split("\\*");
+        arr = string.split("\\*"); //split the string when *
         int multiplication = Arrays.stream(arr).mapToInt(Integer:: parseInt).reduce(1,(a,b)->a*b);
         System.out.println(multiplication);
     }
@@ -32,13 +32,13 @@ public class Operations {
         entry();
         arr = string.split("/");
         Double division = Arrays.stream(arr).mapToDouble(Double:: parseDouble).reduce((a,b)->{
-            if (b == 0.0) {
+            if (b == 0.0) { //catch an 0 and return the division accumulator
                 System.out.println("Error: Division by zero is not supported");
                 return a;
             } else {
                 return a / b;
             }
-        }).orElse(Double.NaN);
+        }).orElse(Double.NaN); //if the string is void
         System.out.println(division);
     }
 }
